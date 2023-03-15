8aba4da501904f20a55fecdc5f707e9545b00bce
eloiselcr <eloise.lecronier@gmail.com>
2023-03-15 17:46:24 +0100

    -> ajout JS

--- CHAP04_JAVASCRIPT/TD_API/Application/traitement.js
+++ CHAP04_JAVASCRIPT/TD_API/Application/traitement.js
@@ -6,15 +6,19 @@ function mafonction(){
     alert("Démarrage de l'API"); // Alerte du lancement de l'API
 
     setInterval(function (){ // EXERCICE 4, Interval fixé à 2sec
+    
 
-    fetch('API_traitement.php').then((resp) => resp.json()).then(
+    fetch('API_traitement.php', {
+        method: 'POST',
+        body: data,
+    }).then((resp) => resp.json()).then(
     // data est la réponse http de notre API
     function (respJson){ // DOM
     console.log(respJson);
     update("arenne", respJson);
     }
     )
-    
+
     .catch(function(error) {
     // This is where you run code if the server returns any errors
     console.log(error);
